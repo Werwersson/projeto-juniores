ed2d3ae2dfa83ec035b3bab0a40a0baefd56db75
Werwersson <113356397+Werwersson@users.noreply.github.com>
2026-08-05 09:32:08 -0300

    Refactor app creation to remove environment variable
    Removed environment variable fetching for FLASK_ENV.

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import os
 from app import create_app
 
-app = create_app(os.environ.get("FLASK_ENV", "development"))
+# Remova o os.environ.get daqui
+app = create_app()
 
 if __name__ == "__main__":
     app.run()
