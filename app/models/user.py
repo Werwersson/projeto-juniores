d8b5f54bff243d3f2b91bd8b3b43123f9462acb3
Werwersson <113356397+Werwersson@users.noreply.github.com>
2026-08-05 09:26:07 -0300

    Update import path for db and login_manager

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 import enum
 import secrets
 from datetime import datetime, timedelta
-from app import db, login_manager
+from app.extensions import db, login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
