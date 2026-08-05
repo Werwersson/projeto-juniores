0f4dbc6657b96f52cead0c4f9e3b1b4d0afc2bd2
Werwersson <113356397+Werwersson@users.noreply.github.com>
2026-08-05 10:10:19 -0300

    Initialize login manager in create_app function
    Added login manager initialization and updated imports.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 import os
 from flask import Flask
-from app.extensions import db
+from app.extensions import db, login_manager  # Importando os dois agora!
 
 def create_app():
     app = Flask(__name__)
     
-    # Adicione esta linha para puxar a URL do banco do ambiente
+    # Configurações básicas (puxando do Vercel ou usando padrão local)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///local.db')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '')
     
+    # Inicializando as extensões com o app
     db.init_app(app)
+    login_manager.init_app(app)
 
+    # Registro de Blueprints
     from app.blueprints.period import period_bp
     app.register_blueprint(period_bp)
     
